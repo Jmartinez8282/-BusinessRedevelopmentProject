bac5dd36d58bfae8b6eec68b2fd1d18e88fe088a
AlexH209 <alexher209@gmail.com>
2019-11-22 10:59:38 -0800

    Commimt

---
+++ script.js
@@ -0,0 +1,6 @@
+let imageoneClick = document.getElementById('imageoneClick');
+let removeEverything = document.getElementById('removeEverything');
+
+imageoneClick.addEventListener('click', function(event){
+    removeEverything.style.display = "none";
+});
